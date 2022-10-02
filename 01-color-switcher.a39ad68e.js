12cae0b8b0a32713e70ea58e8ed7bd2121f4cfca
Denisiimo <104323715+Denisiimo@users.noreply.github.com>
2022-10-02 18:05:48 +0000

    Deploying to gh-pages from @ Denisiimo/goit-js-hw-09@abb8fc5120b99bbbc02dcfba3c0dc83e9fb09068 🚀

--- 01-color-switcher.a39ad68e.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]");t.disabled=!0,e.addEventListener("click",(function(){background=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),e.disabled=!0,t.disabled=!1})),t.addEventListener("click",(function(){clearInterval(background),t.disabled=!0,e.disabled=!1}));
-//# sourceMappingURL=01-color-switcher.a39ad68e.js.map
